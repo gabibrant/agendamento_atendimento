e76b76709d5b68ae44f8ce8f1178b403a2a919bc
paulo-carvalho <paulocarvalho@dcc.ufmg.br>
2016-06-13 19:46:55 -0300

    TecnicoRead.java

--- agendamento_atendimento/src/org/agendamentoatendimento/view/AgendamentoAtendimentoTeste.java
+++ agendamento_atendimento/src/org/agendamentoatendimento/view/AgendamentoAtendimentoTeste.java
@@ -28,20 +28,20 @@ public class AgendamentoAtendimentoTeste {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MenuInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TecnicoRead.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MenuInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TecnicoRead.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MenuInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TecnicoRead.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MenuInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TecnicoRead.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new MenuInicial().setVisible(true);
+                new TecnicoRead().setVisible(true);
             }
         });
     }
